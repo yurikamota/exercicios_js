ba0037b04d9665e103aed794be3c6500446fe9c8
Mac <yurika_mota@outlook.com>
2026-02-17 18:43:29 -0300

    First commit

---
+++ exercicio2.js
@@ -0,0 +1,4 @@
+const anoAtual = new Date().getFullYear();
+const anoNascimento = '1996';
+
+console.log('Você tem ' + (anoAtual - anoNascimento) + ' anos de idade. Seja bem-vindo(a) ao JavaScript!');
