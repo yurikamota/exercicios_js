d731a325a5782fe0ab87712d914c1a0837883a60
Mac <yurika_mota@outlook.com>
2026-02-21 21:28:54 -0300

    add comandos basicos

---
+++ comandos_basicos/coercao_exemplo.js
@@ -0,0 +1,8 @@
+var variavelTexto = "texto";
+var variavelNumero1 = 23;
+var variavelNumero2 = 11;
+
+console.log(variavelTexto + variavelNumero2);
+console.log(variavelNumero1 + variavelNumero2);
+
+// var é antigo evite usar, prefira let ou const
---
+++ comandos_basicos/prompt.js
@@ -0,0 +1,7 @@
+var variavel;
+
+variavel = prompt("Digite um valor para a variável");
+
+alert(variavel)
+
+// inspecione qualquer site, abra o console do navegador (geralmente com F12 ou Ctrl+Shift+I) e cole esse código para testar. Ele irá solicitar que você digite um valor e, em seguida, exibirá esse valor em um alerta.
