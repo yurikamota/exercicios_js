b749233d36648f3820a4bbdb7c53f6d37572d886
Mac <yurika_mota@outlook.com>
2026-02-22 23:03:23 -0300

    add operadores

---
+++ comandos_basicos/operadores.js
@@ -0,0 +1,17 @@
+var somaESubtracao = 10 + 5 - 3;
+var subtracaoEMultiplicacao = 10 - 5 * 3;
+var multiplicacaoEExponenciacao = 10 * 5 ** 2;
+var agrupamentoEDivisao = (10 + 5) / 3;
+
+console.log(somaESubtracao); // 12
+console.log(subtracaoEMultiplicacao); // -5
+console.log(multiplicacaoEExponenciacao); // 250
+console.log(agrupamentoEDivisao); // 5
+
+// Exponenciação **
+// Multiplicação * 
+// Divisão /
+// Resto % (modulo)
+// Adição +
+// Subtração -
+// Precedência: () > ** > * / % > + -
---
+++ exercicios_basicos/soma_basica.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("Digite o valor do primeiro número: "));
+let num2 =  parseFloat(prompt("Digite o valor do segundo número: "));
+let result = num1 + num2
+
+alert("O resultado da soma é: " + result);
+
+// resultado tambem pode ser no console com console.log 
