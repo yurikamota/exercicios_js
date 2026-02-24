17951c712433931fcc916b53f14040437fc643af
Mac <yurika_mota@outlook.com>
2026-02-24 20:56:46 -0300

    add logica_1.js

---
+++ exercicios_basicos/logica_1.js
@@ -0,0 +1,16 @@
+// Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+let A = 5;
+let B = 19;
+let C = 21;
+
+let result = A + B;
+
+console.log("A soma de A e B é:", result);
+
+if (result < C) {
+    console.log("A soma é menor que C.");
+} else {
+    console.log("A soma não é menor que C.");
+}
+
