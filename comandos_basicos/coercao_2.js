641ac774692f3d430c3cb457134e411c97c7b95d
Mac <yurika_mota@outlook.com>
2026-02-24 17:02:24 -0300

    add comentario

--- comandos_basicos/coercao_2.js
+++ comandos_basicos/coercao_2.js
@@ -10,3 +10,5 @@ if (variavelSemCoercao === "37") {
 }
 
 // Somente a linha 5 será executada pois o operador == realiza a comparação com coerção: 37 e "37" são tipos diferentes sendo um número e o outro string. Nesse caso o operador == considerou que apesar de serem tipos diferentes, os valores são equivalentes, então a comparação retornou verdadeiro. Já o operador === realiza a comparação sem coerção, ou seja, ele verifica se os valores são iguais e se os tipos são iguais. Nesse caso, 37 é um número e "37" é uma string, então a comparação retornou falso e o trecho dentro do if não foi executado.
+
+// Prefira o uso de === em vez de ==, pois o uso do segundo pode acabar gerando erros não identificados. 
