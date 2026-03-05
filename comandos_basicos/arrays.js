6a513f5498778fe48d2dbfe69ebc5633f776b25d
Mac <yurika_mota@outlook.com>
2026-03-04 23:29:24 -0300

    add arrays.js

---
+++ comandos_basicos/arrays.js
@@ -0,0 +1,6 @@
+var arrJogadores = ['Ricardo', 'Monica', 'Leonardo'];
+var arrAnoNascimento = newArray(1990, 1985, 1992);
+var arrPontos = [45, 117, 78];
+arrPontos[1] = 71; // Aqui estamos atualizando o valor do segundo elemento 'Monica' do array arrPontos para 71. O índice 1 corresponde ao segundo elemento, já que os índices começam em 0.
+console.log(arrJogadores); // Imprime o array de jogadores no console.
+console.log(arrJogadores[1] + ' tem ' + arrPontos[1] + ' pontos.'); // Aqui estamos acessando o segundo elemento do array arrJogadores (que é 'Monica') e o segundo elemento do array arrPontos (que agora é 71) para criar uma mensagem que indica quantos pontos Monica tem. O resultado será: "Monica tem 71 pontos."
