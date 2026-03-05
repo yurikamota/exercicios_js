16bf485cfe58f46429fde24761e237569662eadb
Mac <yurika_mota@outlook.com>
2026-03-04 23:42:29 -0300

    atualizando arrays.js

--- comandos_basicos/arrays.js
+++ comandos_basicos/arrays.js
@@ -4,3 +4,11 @@ var arrPontos = [45, 117, 78];
 arrPontos[1] = 71; // Aqui estamos atualizando o valor do segundo elemento 'Monica' do array arrPontos para 71. O índice 1 corresponde ao segundo elemento, já que os índices começam em 0.
 console.log(arrJogadores); // Imprime o array de jogadores no console.
 console.log(arrJogadores[1] + ' tem ' + arrPontos[1] + ' pontos.'); // Aqui estamos acessando o segundo elemento do array arrJogadores (que é 'Monica') e o segundo elemento do array arrPontos (que agora é 71) para criar uma mensagem que indica quantos pontos Monica tem. O resultado será: "Monica tem 71 pontos."
+
+// Outro exemplo:
+// newArray(nomejogador, anonascimento, pontos)
+var arrRicardo = ['Ricardo', 1990, 45];
+var arrMonica = ['Monica', 1985, 71];
+var arrLeonardo = ['Leonardo', 1992, 78];
+var arrJogadores = [arrRicardo, arrMonica, arrLeonardo];
+console.log(arrJogadores); // Imprime o array de jogadores, onde cada jogador é representado por um array com seu nome, ano de nascimento e pontos. O resultado será: [["Ricardo", 1990, 45], ["Monica", 1985, 71], ["Leonardo", 1992, 78]].
