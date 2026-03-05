340ab6b028191c1e25705f5a3a124289eb1f8bc5
Mac <yurika_mota@outlook.com>
2026-03-05 10:50:34 -0300

    add unshift

--- comandos_basicos/arrays.js
+++ comandos_basicos/arrays.js
@@ -12,3 +12,15 @@ var arrMonica = ['Monica', 1985, 71];
 var arrLeonardo = ['Leonardo', 1992, 78];
 var arrJogadores = [arrRicardo, arrMonica, arrLeonardo];
 console.log(arrJogadores); // Imprime o array de jogadores, onde cada jogador é representado por um array com seu nome, ano de nascimento e pontos. O resultado será: [["Ricardo", 1990, 45], ["Monica", 1985, 71], ["Leonardo", 1992, 78]].
+
+// Função push para adicionar um novo elemento ao array:
+
+var arrPrato = ['Arroz', 'Feijão', 'Carne'];
+arrPrato.push('Salada'); // Aqui estamos usando o método push para adicionar o elemento 'Salada' ao final do array arrPrato. O array agora contém ['Arroz', 'Feijão', 'Carne', 'Salada'].
+console.log(arrPrato); // Imprime o array atualizado de pratos no console. O resultado será: ['Arroz', 'Feijão', 'Carne', 'Salada'].
+
+// Unshift adiciona um elemento no início do array:
+
+var arrEmenta = ['Matematica', 'Português', 'História'];
+arrEmenta.unshift('Geografia'); // Aqui estamos usando o método unshift para adicionar o elemento 'Geografia' no início do array arrEmenta. O array agora contém ['Geografia', 'Matematica', 'Português', 'História'].
+console.log(arrEmenta); // Imprime o array atualizado de ementa no console. O resultado será: ['Geografia', 'Matematica', 'Português', 'História'].
