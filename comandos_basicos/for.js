8254e073150732c077d0272fff747bf70f7e082b
Mac <yurika_mota@outlook.com>
2026-03-30 15:45:23 -0300

    add for.js

---
+++ comandos_basicos/for.js
@@ -0,0 +1,9 @@
+// for (contador; condição; incremento) 
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// O código acima é um exemplo de um loop for, onde a variável i é inicializada com 0, 
+// a condição é que i seja menor que 10, e o incremento é feito com i++. 
+// A cada iteração do loop, o valor de i é impresso no console. 
+// O loop continuará executando até que a condição seja falsa, ou seja, quando i atingir 10.
