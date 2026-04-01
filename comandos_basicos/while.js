dc60556438618df6ef40a3c0a592ef6241be8511
Mac <yurika_mota@outlook.com>
2026-03-31 21:24:07 -0300

    add correcao

--- comandos_basicos/while.js
+++ comandos_basicos/while.js
@@ -1,11 +1,12 @@
 var nomes = ['Soluço', 'Astrid', 'Banguela', 'Meatlug', 'Bocão'];
 var nomePesquisado = null;
-while (nomePsquisado== null && i < nomes.length) {
+var i = 0;
+while (nomePesquisado== null && i < nomes.length) {
     if (nomes[i] == 'Banguela') {
         nomePesquisado = nomes[i];
 } else {
     console.log(nomes[i] + ' não é o nome procurado.');
 }
 i++;
-
-console.log('Nome encontrado: ' + nomePesquisado);}
+}
+console.log('Nome encontrado: ' + nomePesquisado);
