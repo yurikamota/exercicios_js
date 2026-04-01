ba4362711c3a97cb2598e262252f6438711b3177
Mac <yurika_mota@outlook.com>
2026-03-31 21:25:21 -0300

    add comentarios

--- comandos_basicos/while.js
+++ comandos_basicos/while.js
@@ -10,3 +10,11 @@ while (nomePesquisado== null && i < nomes.length) {
 i++;
 }
 console.log('Nome encontrado: ' + nomePesquisado);
+
+// O código acima é um exemplo de um loop while que percorre um array de nomes em busca de um nome específico, "Banguela".
+// A variável nomePesquisado é inicialmente definida como null, e a variável i é usada como índice para percorrer o array.
+// O loop while continua executando enquanto nomePesquisado for null e i for menor que o comprimento do array de nomes.
+// Dentro do loop, há uma condição if que verifica se o nome atual (nomes[i]) é igual a "Banguela". 
+// Se for, nomePesquisado é atualizado com o nome encontrado. Caso contrário, uma mensagem é impressa indicando que o nome atual não é o procurado.
+// O índice i é incrementado a cada iteração para avançar para o próximo nome no array.
+// Após o loop, o nome encontrado é impresso no console.
