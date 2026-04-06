0c019a9a3a5196b8ab3972d2adfedb2e95b01181
Mac <yurika_mota@outlook.com>
2026-04-06 20:37:03 -0300

    add break.js

---
+++ comandos_basicos/break.js
@@ -0,0 +1,9 @@
+var animaisMarinhos = ['golfinho', 'baleia', 'tubarão', 'polvo', 'estrela-do-mar'];
+for (var i = 0; i < animaisMarinhos.length; i++) {
+    if (animaisMarinhos[i] == 'tubarão') {
+        console.log(animaisMarinhs[i] + ' encontrado, interrompendo o loop.');
+        break;
+    } else {
+        console.log('Procurar animal marinho.');
+    }
+}
