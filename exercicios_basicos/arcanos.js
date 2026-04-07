649ca70ccb4c84b141ee2e0d1d2fcaeffc7c355d
Mac <yurika_mota@outlook.com>
2026-04-06 21:24:53 -0300

    add mensagens para arcanos

--- exercicios_basicos/arcanos.js
+++ exercicios_basicos/arcanos.js
@@ -3,3 +3,19 @@ let arcanoDoDia = ['O Mago', 'A Sacerdotisa', 'A Imperatriz', 'O Imperador', 'O 
 let arcanoSorteado = arcanoDoDia[Math.floor(Math.random() * arcanoDoDia.length)];
 
 console.log("O seu arcano do dia é:", arcanoSorteado);
+
+// adicionando mensagens para cada arcano, por exemplo:
+switch (arcanoSorteado) {
+    case 'O Mago':
+        console.log("Hoje é um dia de criatividade e novas oportunidades. Aproveite para iniciar projetos e usar sua habilidade de manifestar seus desejos.");
+        break;
+    case 'A Sacerdotisa':
+        console.log("Hoje é um dia para confiar na sua intuição e buscar conhecimento interior. Ouça sua voz interior e siga seu coração.");
+        break;
+    case 'A Imperatriz':
+        console.log("Hoje é um dia para se conectar com a natureza e cultivar a beleza ao seu redor. Aproveite para cuidar de si mesmo e dos outros.");
+        break;
+
+    default:
+        console.log("Tenha um ótimo dia, independentemente do arcano sorteado!");
+}
