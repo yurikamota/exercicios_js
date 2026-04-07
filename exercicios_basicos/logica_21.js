bea8a9375c070ca8641497014bc1a2814558d026
Mac <yurika_mota@outlook.com>
2026-04-06 21:13:37 -0300

    logica_21.js

---
+++ exercicios_basicos/logica_21.js
@@ -0,0 +1,5 @@
+// 21 - Faça um algoritmo que mostre um valor aleatório entre 0 e 100.
+
+let numeroSorteado = Math.floor(Math.random() * 100) + 1;
+
+console.log("Número sorteado:", numeroSorteado);
