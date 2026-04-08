5f7507b722dd308a5b9e06a35d1d1707383713b7
Mac <yurika_mota@outlook.com>
2026-04-07 21:49:44 -0300

    add calculadora_IMC

--- exercicios_basicos/logica_24.js
+++ exercicios_basicos/logica_24.js
@@ -1,6 +1,17 @@
+/* 24 - Faça um algoritmo que calcule a quantidade de litros de combustível gastos em uma viagem, 
+sabendo que o carro faz 12km com um litro. Deve-se fornecer ao usuário o tempo que será gasto na viagem a sua 
+velocidade média, distância percorrida e a quantidade de litros utilizados para fazer a viagem.
+
+Fórmula: distância = tempo x velocidade.
+
+            litros usados = distância / 12.
+*/
+
+
+
 let tempo = parseFloat(prompt("Digite o tempo de viagem (em horas): "));
 let velocidade = parseFloat(prompt("Digite a velocidade média (em km/h): "));
-
+// 1 litro de combustível é gasto a cada 12 km percorridos.
 let distancia = velocidade * tempo;
 let litrosConsumidos = distancia / 12;
 
