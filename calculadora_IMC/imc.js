a09de69eb9850749382edb33f49cb33cda9237eb
Mac <yurika_mota@outlook.com>
2026-04-09 20:46:20 -0300

    update imc.js

--- calculadora_IMC/imc.js
+++ calculadora_IMC/imc.js
@@ -1,11 +1,31 @@
-const nome = document.getElementById('nome');
-const peso = document.getElementById('peso');
-const altura = document.getElementById('altura');
 const calcular = document.getElementById('calcular');
-const resultado = document.getElementById('resultado');
+
 
 function imc() {
-    alert ('teste click');
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && peso !== '' && altura !== '') {
+       const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+       let classificacao = '';
+
+       if (valorIMC < 18.5) {
+           classificacao = 'Abaixo do peso';
+       } else if (valorIMC >= 18.5 && valorIMC < 25) {
+           classificacao = 'Peso normal';
+       } else if (valorIMC >= 25 && valorIMC < 30) {
+           classificacao = 'Sobrepeso';
+       } else {
+           classificacao = 'Obesidade';
+       }
+
+       resultado.textContent = `${nome}, seu IMC é ${valorIMC} (${classificacao})`;
+    } else {
+        resultado.textContent = 'Preencha todos os campos!';
+    }
 }
 
 calcular.addEventListener('click', imc);
