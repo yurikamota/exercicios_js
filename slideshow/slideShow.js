b9f0b98a796b44f719f505a74d294bd403402ef7
Mac <yurika_mota@outlook.com>
2026-04-20 20:21:34 -0300

    slideshow

---
+++ slideshow/slideShow.js
@@ -0,0 +1,14 @@
+'use strict'; // serve para garantir que o código seja executado em modo estrito, o que ajuda a evitar erros comuns e a melhorar a segurança do código.
+
+const images = [
+    { 'id' : '1', 'url' : '.img/chrono.jpg'},
+    { 'id' : '2', 'url' : '.img/inuyasha.jpg'},
+    { 'id' : '3', 'url' : '.img/ippo.png'},
+    { 'id' : '4', 'url' : '.img/tenchi.jpg'},
+    { 'id' : '5', 'url' : '.img/tenjhotenge.jpg'},
+    { 'id' : '6', 'url' : '.img/yuyuhakusho.jpg'},
+
+]
+
+
+loadImages( images ); 
