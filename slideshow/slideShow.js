ec8a5734c35d718593f7601145ad70f06450fdde
Mac <yurika_mota@outlook.com>
2026-04-20 21:14:47 -0300

    style.css

--- slideshow/slideShow.js
+++ slideshow/slideShow.js
@@ -1,14 +1,24 @@
-'use strict'; // serve para garantir que o código seja executado em modo estrito, o que ajuda a evitar erros comuns e a melhorar a segurança do código.
+'use strict';
 
 const images = [
-    { 'id' : '1', 'url' : '.img/chrono.jpg'},
-    { 'id' : '2', 'url' : '.img/inuyasha.jpg'},
-    { 'id' : '3', 'url' : '.img/ippo.png'},
-    { 'id' : '4', 'url' : '.img/tenchi.jpg'},
-    { 'id' : '5', 'url' : '.img/tenjhotenge.jpg'},
-    { 'id' : '6', 'url' : '.img/yuyuhakusho.jpg'},
+    { 'id': '1', 'url': './img/chrono.jpg' },
+    { 'id': '2', 'url': './img/inuyasha.jpg' },
+    { 'id': '3', 'url': './img/ippo.png' },
+    { 'id': '4', 'url': './img/tenchi.jpg' },
+    { 'id': '5', 'url': './img/tenjhotenge.jpg' },
+    { 'id': '6', 'url': './img/yuyuhakusho.jpg' },
+];
 
-]
+const container = document.querySelector('#container-items');
 
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}' alt='${image.id}' />
+            </div>
+        `;
+    });
+};
 
-loadImages( images ); 
+ loadImages(images, container);
